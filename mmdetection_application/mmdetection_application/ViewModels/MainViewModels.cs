3b61810d1a7bb6e33f97f5f4480ce4d6137b4e07
BuildTools <unconfigured@null.spigotmc.org>
2024-03-21 19:45:59 +0900

    INorifyPropertyChangedを継承したMainViewsModelクラスの実装

--- mmdetection_application/mmdetection_application/ViewModels/MainViewModels.cs
+++ mmdetection_application/mmdetection_application/ViewModels/MainViewModels.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace mmdetection_application.ViewModels
 {
-    class MainViewModels : NotificationObject
+    public class MainViewModels : INotifyPropertyChanged
     {
-        /// <summary>
-        /// MainVMのコンストラクタ
-        /// </summary>
-        public MainViewModels()
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string propertyName)
         {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
