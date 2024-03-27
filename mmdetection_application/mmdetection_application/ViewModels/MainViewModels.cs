dd328783406c588b46268c393102bb37ad0c4184
Akira Izumi <a-izumi@shibuya.co.jp>
2024-03-27 15:28:56 +0900

    INotifyPropertyChangedを継承したクラスの修正

--- mmdetection_application/mmdetection_application/ViewModels/MainViewModels.cs
+++ mmdetection_application/mmdetection_application/ViewModels/MainViewModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,19 +10,24 @@ namespace mmdetection_application.ViewModels
 {
     public class MainViewModels : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void RaisePropertyChanged(string propertyName)
+        #region InotifyPropertyChanged の実装
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
         {
-            var handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
+            var h = this.PropertyChanged;
+            if (h != null) h(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public MainViewModels()
+        protected bool SetProperty<T>(ref T target, T value, [CallerMemberName] string? propertyName = null)
         {
+            if (Equals(target, value))
+                return false;
+            target = value;
+            RaisePropertyChanged(propertyName);
+            return true;
         }
-        public string Title { get; private set; }
+        #endregion INotifyPropertyChanged の実装
     }
 }
