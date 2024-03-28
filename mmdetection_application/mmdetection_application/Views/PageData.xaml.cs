2c24c2b958202ccb645483596b29391bf745fc23
Akira Izumi <a-izumi@shibuya.co.jp>
2024-03-28 14:04:02 +0900

    PageDataのコードビハインドに機能の実装（ファイルを開くダイアログの追加）

--- mmdetection_application/mmdetection_application/Views/PageData.xaml.cs
+++ mmdetection_application/mmdetection_application/Views/PageData.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using mmdetection_application.ViewModels;
 
 namespace mmdetection_application.Views
 {
@@ -23,6 +26,23 @@ namespace mmdetection_application.Views
         public PageData()
         {
             InitializeComponent();
+            _vm = new DataViewModel();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var file_dlg = new OpenFileDialog();
+
+            file_dlg.Filter = "アノテーションファイル (*.json)|*.json";
+
+            if (file_dlg.ShowDialog() == true)
+            {
+                MessageBox.Show(file_dlg.FileName);
+            }
+
+            _vm.NameOpenFile = file_dlg.FileName;
+        }
+
+        DataViewModel _vm;
     }
 }
